28f6f819c3c464ee5f246ef8c13c2faed1a25c4a
anish.singh <anish.singh@N12940.intellectdesign.com>
2021-09-24 08:52:29 +0530

    anish commit

--- books/src/mybooks/bookMain.java
+++ books/src/mybooks/bookMain.java
@@ -23,7 +23,8 @@ public class bookMain {
 				do
 				{
 				System.out.println("1. Adding Books\n2. Search Books\n3. Update Book\n4. Delete Book");
-				System.out.println("Choose one option");
+				
+				System.out.println("chosse any one option");
 				int ch = sc.nextInt();
 				
 				if(ch==1)
